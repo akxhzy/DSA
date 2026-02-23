489e1b37c1b44ee4e0c79d4b436fa0e63a7fecd7
Akshay <theakshayroy@gmail.com>
2026-02-23 21:54:39 +0600

    Time: 0 ms (100.00%) | Memory: 12.8 MB (18.63%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.c
@@ -0,0 +1,23 @@
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    dummy.next = NULL;
+    struct ListNode* curr = &dummy;
+    int carry = 0;
+
+    while (l1 || l2 || carry) {
+        int sum = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+        carry = sum / 10;
+        
+        // Allocate memory for the new node
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10;
+        newNode->next = NULL;
+        
+        curr->next = newNode;
+        curr = curr->next;
+        
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
+    }
+    return dummy.next;
+}
